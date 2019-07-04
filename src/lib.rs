14377fc688e397ec3e48b14836e9eed31547ac07
twm <atharvs.twm@gmail.com>
2019-07-05 00:43:54 +0530

    update code using iterators

--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,19 @@ pub struct Config {
 }
 
 impl Config {	
-	pub fn new(args: &Vec<String>) -> Result<Config, &'static str> {	
-		if args.len() < 3 {
-			return Err("minigrep takes at least two arguments!")
-		}
+	pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {	
+		args.next();
 
-		let query = args[1].clone();
-		let filename = args[2].clone();
+		let query = match args.next() {
+			Some(arg) => arg,
+			None => return Err("Didn't get a query string!"),
+		};	
+
+		let filename = match args.next() {
+			Some(arg) => arg,
+			None => return Err("Didn't get a input filename!"),
+		};
+
 		let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
 		Ok(Config{ query, filename, case_sensitive })
@@ -40,14 +46,9 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
 
 pub fn case_sensitive_search<'a>(query: &str, contents:&'a str) -> Vec<&'a str> {
-	let mut result: Vec<&str> = Vec::new();
-	for line in contents.lines() {
-		if line.contains(query) {
-			result.push(line);
-		}
-	}
-
-	result
+	contents.lines()
+		.filter(|line| line.contains(query))
+		.collect()
 }
 
 
